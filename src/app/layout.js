4f4c8057fa50f829b79af86dece2b98423fc92b6
lazim mv <lazimlatheef@gmail.com>
2024-03-12 15:13:42 +0530

    v0.02 metaDataAdded

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,7 +31,7 @@ export default function RootLayout({ children }) {
 
         <meta
           property="og:image"
-          // content="https://bmcglobal.co/linkImageWhatsApp.png"
+          content="https://loopify-zeta.vercel.app/openGrahpImage.jpg"
         />
         <meta property="og:image:type" content="image/png" />
         <meta property="og:image:width" content="1024" />
